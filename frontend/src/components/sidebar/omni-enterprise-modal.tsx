fd9d5455716cbf8c555d26d5cd07939fc17ef21e
vchabria <vchabria@purdue.edu>
2025-09-24 22:03:39 -0400

    add comment to enterprise modal component

--- frontend/src/components/sidebar/omni-enterprise-modal.tsx
+++ frontend/src/components/sidebar/omni-enterprise-modal.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+// Enterprise modal component for scheduling demos and consultations
 import React, { useState, useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import {
